c16f94f7cdc2024242aeb36a17d49f19ed347e6c
Zebin Zhou <zb.zhou0922@gmail.com>
2022-01-21 13:07:01 -0800

    Add check file exist

--- products.py
+++ products.py
@@ -1,12 +1,18 @@
+import os # operating system
+
 # Read File
 products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if 'Item, Price' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'): # Check whether file exist
+	print('Yeah! Find the file!')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if 'Item, Price' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('No such a file.')
 
 # Prompt User to input
 while True:
